02d330fb27fa453d10e648b428d754d238f0219e
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2025-05-27 06:58:34 +0000

    Simple i/o

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
+// std::string readInput(std::string &prompt) {
+// 	std::cout << *prompt; // Shell prompt
+// 	std::string input;
+// 	std::getline(std::cin, input);
+// 	return input;
+// }
+
+std::string readInput() {
+	std::cout << "> "; // Shell prompt
+	std::string input;
+	std::getline(std::cin, input);
+	return input;
+}
 
 int main() {
-	std::cout << "Welcome to Mini Shell!" << std::endl;
+	std::string input;
+
+	while (true) {
+		input = readInput();
+
+		if (input == "exit") {
+			std::cout << "Exiting shell...\n";
+			break;
+		}
+
+		std::cout << "Input received: " << input << std::endl;
+	}
+
 	return 0;
 }
